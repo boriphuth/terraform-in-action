13f702fb55cc9ada0629caefedb9aaefe5730b33
Krzysztof Madej <krzysztof.madej@hotmail.com>
2019-12-07 07:14:47 +0100

    Code for chapter 5

---
+++ Chapter_5/variables.tf
@@ -0,0 +1,25 @@
+variable "subscription_id" { 
+  type = string 
+} 
+
+variable "client_id" { 
+  type = string 
+} 
+
+variable "tenant_id" { 
+  type = string 
+} 
+
+variable "client_secret" { 
+  type = string 
+} 
+ 
+variable "region" { 
+  type    = string 
+  default = "westeurope" 
+} 
+ 
+variable "namespace" { 
+  type    = string 
+  default = "ballroominaction" 
+} 
