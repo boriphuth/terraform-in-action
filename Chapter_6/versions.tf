f4037eab02603c6fdab6f1b30ff2a8891e5e4b3b
Krzysztof Madej <krzysztof.madej@hotmail.com>
2019-12-24 00:26:13 +0100

    Code for Chapter 6

---
+++ Chapter_6/versions.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = "~> 0.12"
+  required_providers {
+    aws = "~> 2.19"
+    random = "~> 2.1"
+  }
+}
+
+#Documentation generator for terraform https://github.com/segmentio/terraform-docs
